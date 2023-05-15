b7478197fba4e45e1d1da8668ed6bdc40bb39414
Nagesha ks <109476085+NAGESHGOWDA1993@users.noreply.github.com>
2023-05-15 23:46:59 +0100

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,20 @@
+#creating EC2 instance
+resource "aws_instance" "Nagesh-ec2" {
+  ami                         = "ami-0432c2005d3e6a7f4" #Amazon linux 2 AMI
+  key_name                    = "kmaster"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public-subnet.id
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.lab-Public-SG.id]
+  user_data                   = <<-EOF
+        #!/bin/bash
+        yum update -y
+        yum install httpd -y
+        systemctl start httpd
+        systemctl enable httpd
+        echo "<html><body><h1>This is Nagesh lab Project Tier 1 </h1></body></html>" > /var/www/html/index.html
+        EOF
+  tags = {
+    Name = "nagesh-EC2"
+  }
+}
